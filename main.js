a652ab170b189f1ebc2569aee0de1d20d83f0580
anafilipadealmeida <anafilipadealmeida@gmail.com>
2017-05-02 14:50:26 +0100

    Add js with behaviour for key operator

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+var tmpResult = 0;
+var tmpOperator;
 $(".calc__key").click(function() {
-  var newInput = $(this).text();
-  var oldInput = $(".calc__display").text();
-  if (oldInput === "0") {
-    $(".calc__display").text(newInput);
-  }
-  else {
-    $(".calc__display").text(oldInput + newInput);
-  }
+	var newInput = $(this).text();
+	var oldInput = $(".calc__display").text();
+
+	if ($(this).hasClass("calc__key--operator")) {
+		tmpResult = parseInt(oldInput);
+	 	tmpOperator = newInput;
+		$(".calc__display").text("");
+	}
+	else {
+
+		if (oldInput === "0") {
+	 		$(".calc__display").text(newInput);
+		}
+	  	else {
+			$(".calc__display").text(oldInput + newInput);
+		}
+  	}
 });
