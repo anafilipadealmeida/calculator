227a8a6e1a84d0bbe23328467df5d3be389cf035
anafilipadealmeida <anafilipadealmeida@gmail.com>
2017-05-02 15:25:15 +0100

    Add Arithmetic operators to the calculator

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 var tmpResult = 0;
 var tmpOperator;
+var operation;
 $(".calc__key").click(function() {
 	var newInput = $(this).text();
 	var oldInput = $(".calc__display").text();
 
 	if ($(this).hasClass("calc__key--operator")) {
 		tmpResult = parseInt(oldInput);
-	 	tmpOperator = newInput;
+		tmpOperator = newInput;
 		$(".calc__display").text("");
 	}
 	else if($(this).hasClass("calc__key--total")) {
-		$(".calc__display").text(tmpResult + parseInt(oldInput));
+		switch (tmpOperator) {
+			case '+':
+				operation = (tmpResult + parseInt(oldInput));
+				break;
+			case '-':
+				operation = (tmpResult - parseInt(oldInput));
+				break;
+			case 'x':
+				operation = (tmpResult * parseInt(oldInput));
+				break;
+			case '/':
+				operation = (tmpResult / parseInt(oldInput));
+				break;
+		}
+		$(".calc__display").text(operation);
 	}
 
 	else {
 
 		if (oldInput === "0") {
-	 		$(".calc__display").text(newInput);
+			$(".calc__display").text(newInput);
 		}
-	  	else {
+		else {
 			$(".calc__display").text(oldInput + newInput);
 		}
-  	}
+	}
 });
